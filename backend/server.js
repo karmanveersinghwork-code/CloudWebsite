baa4c1b5dadeaa8eb97aca61c9ed736dda1829a2
Karman <karmanveersinghwork@gmail.com>
2026-02-25 02:25:55 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/auth');
 const fileRoutes = require('./routes/files');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
